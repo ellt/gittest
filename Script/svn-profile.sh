bb9b17fbc823a6ba292535970aa5c60c1c8c9236
agz1990 <522360568@qq.com>
2014-11-25 01:15:14 +0800

    add script

--- Script/svn-profile.sh
+++ Script/svn-profile.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+SVN_INFO_FILE='./wwwroot/.svn'
+
 echo "########################################"
+
+CUR_SVN_DIR="./Svndir/"$(git branch | awk '/^\*/{print $2}')".svn"
+
+echo "CUR_SVN_DIR: $CUR_SVN_DIR"
+
+rm $SVN_INFO_FILE -rf && cp $CUR_SVN_DIR $SVN_INFO_FILE -rf
