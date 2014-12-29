3289055ab5036eb5bc26b50474b05a05346ffb70
jigc <mrji1990@gmail.com>
2014-12-30 03:56:10 +0800

    1. 用户逻辑初步分层次， 功能未实现，仅做占坑用！

---
+++ wwwroot/Application/Common/Model/UserModel.class.php
@@ -0,0 +1,31 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | ELangLang  [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2014 http://www.elanglang.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: jigc <mrji1990@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace Common\Model;
+use Think\Model\RelationModel;
+
+
+/**
+ * 用户模型定义
+ * Class UserModel
+ * @package Home\Model
+ */
+class UserModel extends RelationModel
+{
+
+    /**
+     * @var array
+     */
+    public $_link = array(
+
+    )
+
+
+}
