c4d636210bcfb73cf1761a7b0e8212165fc17d8c
Guokaiyi <Harrygky@gmail.com>
2015-01-01 09:52:42 +0800

    【增加】【用户中心】【基础数据】 1、增加前端模板

---
+++ wwwroot/Application/Usercenter/Controller/UserController.class.php
@@ -0,0 +1,64 @@
+<?php
+// +----------------------------------------------------------------------
+// | ELangLang  [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2014 http://www.elanglang.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: Guokaiyi <harrygky@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace Usercenter\Controller;
+
+/**
+ * 用户控制器
+ */
+class UserController extends AdminController {
+
+    public function index(){
+        $this->redirect('baseSetting');
+    }
+
+    public function baseSetting(){
+
+        if (IS_AJAX) {
+            static $classes = 2;
+            $grade = I('grade');
+            $status = I('status');
+
+            if ($result = $classes + $status) {
+                $classes = $result;
+                $data['status']  = 1;
+                $data['grade'] = $grade;
+                $data['classes'] = $classes;
+            } else {
+                $data['status']  = 0;
+            }
+
+            $this->ajaxReturn($data);
+        }
+
+        $sidemenu['title'] = "基础信息设置";
+        $this->assign('sidemenu', $sidemenu);
+        $this->display();
+    }
+
+    public function addGrade() {
+
+        if (IS_AJAX) {            
+            $grade = I('grade');
+            $gradename = I('gradename');
+            $classes = I('classes');
+
+            if ($result = $classes + $status) {
+                $classes = $result;
+                $data['status']  = 1;
+                $data['grade'] = $grade;
+                $data['classes'] = $classes;
+            } else {
+                $data['status']  = 0;
+            }
+            $this->success("成功". " grade=".grade." gradename=".gradename." classes=".classes, 'refresh');
+            //$this->ajaxReturn($data);
+        }
+    }
+}
