45b7f8ef740012416db5ab544909764b63897f38
jigc <mrji1990@gmail.com>
2015-02-25 15:01:05 +0800

    1. 添加科目新增编辑功能

--- wwwroot/Application/Common/Model/SubjectInfoModel.class.php
+++ wwwroot/Application/Common/Model/SubjectInfoModel.class.php
@@ -15,13 +15,15 @@ class SubjectInfoModel  extends Model{
     
     /* 数据表前缀 */ 
     protected $tablePrefix      =   'school_';
+    
+    protected $patchValidate    =   TRUE;
 
     /* 配置自动验证规则 */
     protected $_validate = array(
         array('subject_number', '', '科目编号冲突', self::MUST_VALIDATE, 'unique', self::MODEL_BOTH ),
+        array('subject_number','/^[0-9]+$/', '科目编号只能是数字', self::VALUE_VALIDATE , 'regex'  ,self::MODEL_BOTH ),
         array('subject_name', '', '科目名称冲突', self::MUST_VALIDATE, 'unique', self::MODEL_BOTH ),
-       
-            
+        
      );
     
     /* 配置字段映射 */
@@ -29,4 +31,51 @@ class SubjectInfoModel  extends Model{
       'number' => 'subject_number',  
       'name' => 'subject_name',
     );
+    
+    
+    public function checkData($data) {
+    
+        $relation_data = false;
+        $this->error = null;
+        if (empty($data)) {
+            $data = I('post.');
+        }
+        
+        
+        /* 获取数据对象 */
+        $data = $this->create($data);
+        return $data;
+    }
+
+    public function update($data) {
+        $update_success_id = 0;
+        $this->error = null;
+        if (empty($data)) {
+            $data = I('post.');
+        }
+        
+        $data = $this->checkData($data);
+        if ($data === false) { // 数据检查失败
+            return false;
+        }
+//         dump('empty($data[id]):' . empty($data['id']));die();
+        if (empty($data['id']) || $data['id'] <= 0) {
+            $update_success_id = $this->add($data);
+            if (!$update_success_id) {
+                return false;
+            }
+        } else {
+            $update_success_id = $this->save($data);
+            if (false === $update_success_id) {
+                return false;
+            } else {
+                $update_success_id = $data['id'];
+            }
+        }
+        return $update_success_id;
+    }
+    
+    public function getSubjectInfoById($id){
+        return $this->where("id=$id")->find();
+    }
 } 
--- wwwroot/Application/Usercenter/Controller/SubjectController.class.php
+++ wwwroot/Application/Usercenter/Controller/SubjectController.class.php
@@ -29,38 +29,43 @@ class SubjectController extends UserCenterController {
         $this->display();
     }
 
-    public function add() {
+    public function setSubjectInfo() {
         if (IS_AJAX) {
             $info = $this->model->create();
-            if (!$info) { // 数据格式不合法
-                $data['status'] = ERROR_OK;
-                /* TODO 数据不合法仅给出错误的提示， 具体要提示到哪个错误，需通过前端表单提交前自行判定 */ 
-                $data['error_info'] = $this->model->getError();
+            
+            if ($this->model->update() == false) { // 数据格式不合法
+                $data['status'] = 0;
+                $dbErrorMsg = $this->model->getError();
+                foreach ($dbErrorMsg as $k => $v) {
+                    $uiErrorMsg[$k]['errorInfo'] = $v;
+                }
+                
+                $data['status'] = 0;
+                $data['hint'] = $uiErrorMsg;
             } else {
-                $this->model->add();
-                $data['status'] = ERROR_OK;
+                $data['info'] = "保存成功！";
+                $data['url'] = "refresh";
+                $data['status'] = 1;
             }
             $this->ajaxReturn($data);
         }
     }
 
-    public function saveSubject() {
+    public function getSubjectInfo(){
         if (IS_AJAX) {
-            $data['status']  = 1;
-            $data['info'] = "保存成功！";
-            $data['url'] = "no-refresh";
-            $this->ajaxReturn($data);
-        }
-    }
-
-    public function classManager(){
-        if (IS_AJAX) {
-            $data['status']  = 1;
-            $data['info'] = "保存成功！";
+            $id = I('id');
+            $subjectInfo = $this->model->getSubjectInfoById($id);
+            if ($subjectInfo == false) {
+                $data['status'] = 0;
+                $data['info'] = '获取科目信息失败';
+            } else {
+                $data['status'] = 1;
+                $data['data'] = $subjectInfo;
+            }
+            
             $data['url'] = "refresh";
             $this->ajaxReturn($data);
         }
-        $this->display();
     }
     
 }
