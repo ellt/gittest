5b8e2256e826500aef12d2c40da060abf0057434
Guokaiyi <Harrygky@gmail.com>
2015-04-11 11:35:38 +0800

    1、调整布置作业页面

--- wwwroot/Public/Teacher/js/homework.js
+++ wwwroot/Public/Teacher/js/homework.js
@@ -1,5 +1,9 @@
 //dom加载完成后执行的js
 $(function() {
+    // 初始化弹出框
+    $('[data-toggle="popover"]').popover();
+
+    // 上一次作业
     $("[data-target='class']").change(function() {
         var history = $("[id^='history']");
         if(this.checked) {
@@ -21,25 +25,57 @@ $(function() {
         });
     });
 
-    $("[data-form-btn='set-time']").click(function() {
-        var targetInput = $(this).attr('data-target');
-        var input = $(targetInput);
-        var content = $(this).attr('data-content');
-        input.val(content);
-    });
-
+    // 作业内容
     $("#homework").bind('input propertychange', function() {
-        var btn = $("[data-submit='form']");
         var text = $(this).val();
+        var $btn = $("[data-submit='form']");
+        var $setTime = $("#setTime");
+        var $setTimeGroup = $setTime.parents(".form-group");
 
         if (text === "") {
             // 没作业
-            btn.html("今天没作业");
-            btn.removeClass("btn-primary").addClass("btn-warning");
+            $btn.html("今天没作业").prop('disabled', false);
+            $btn.removeClass("btn-primary").addClass("btn-warning");
+            $btn.removeClass("btn-primary").addClass("btn-warning");
+            $setTimeGroup.addClass("hidden");
+            $setTime.val(0);
+            showPopTip($("#addHomeworkForm"));
         } else {
             // 有作业
-            btn.html("发布作业");
-            btn.removeClass("btn-warning").addClass("btn-primary");
+            $btn.html("发布作业");
+            $btn.removeClass("btn-warning").addClass("btn-primary");
+            $setTimeGroup.removeClass("hidden");
+            $setTime.val(30);
+        }
+    });
+
+    // 建议时间 设置快捷键
+    $("[data-form-btn='set-time']").click(function() {
+        var targetInput = $(this).attr('data-target');
+        var content = $(this).attr('data-content');
+        var $input = $(targetInput);
+        $input.val(content);
+        $input.trigger("input").trigger("propertychange");
+    });
+
+    // 建议时间 输入框
+    $("#setTime").bind('input propertychange', function() {
+        var val = $(this).val();
+        var $btn = $("[data-submit='form']");
+        var $form = $("#addHomeworkForm");
+
+        if (isInt(val) && 0 < val && val <= 1000) {
+            // 输入符合 1 ~ 1000 的条件
+            showPopTip($form); // 清除弹出框
+            $btn.prop('disabled', false);
+        } else {
+            var tip = {
+                suggest_time: {
+                    errorInfo : "请输入 1 ~ 1000 之间的分钟数"
+                }
+            };
+            showPopTip($form, tip);
+            $btn.prop('disabled', true);
         }
     });
 
@@ -48,21 +84,14 @@ $(function() {
         e.preventDefault();
 
         var url = $(this).attr('data-url');
-        var text = $("#homework").val();
-
-        // 没作业
-        if (text === "") {
-            $("#setTime").val(0); // 将建议用时清零
-        } 
-
         var targetForm = $(this).attr('data-target');
         var content = $(targetForm).serialize();
 
-        var btn = $(this);
-        btn.button('loading');
+        var $btn = $(this);
+        $btn.button('loading');
         //发送到服务器
         $.post(url, content).success(function(data) {
-            btn.button('reset');
+            $btn.button('reset');
             handleAjax(data);
         });
     });
